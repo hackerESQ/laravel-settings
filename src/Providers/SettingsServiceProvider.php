e18938f26a385a104540a7df1c7d9f7da3f25670
hackerESQ <corey@coreyvarma.com>
2018-09-26 15:48:05 -0500

    Update SettingsServiceProvider.php

--- src/Providers/SettingsServiceProvider.php
+++ src/Providers/SettingsServiceProvider.php
@@ -28,7 +28,7 @@ class SettingsServiceProvider extends ServiceProvider {
         /**
 		 * Register migrations
 		 */
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations/create_settings_table.php');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
 
 		/**
 		 * Settings API route
