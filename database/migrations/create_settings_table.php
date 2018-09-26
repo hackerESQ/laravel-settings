65098cdfcb98e6d67c240b5de59f22bf4f3f1945
hackerESQ <corey@coreyvarma.com>
2018-09-26 17:20:37 -0500

    make key unique

--- database/migrations/create_settings_table.php
+++ database/migrations/create_settings_table.php
@@ -14,7 +14,7 @@ class CreateSettingsTable extends Migration
     public function up()
     {
         Schema::create('settings', function (Blueprint $table) {
-            $table->string('key');
+            $table->string('key')->unique;
             $table->text('value')->nullable();
             $table->primary('key');
         });
