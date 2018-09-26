3490fc15c51f32ed45dffa6ba5228a39361c7a9b
hackerESQ <corey@coreyvarma.com>
2018-09-26 15:59:39 -0500

    Update create_settings_table.php

--- database/migrations/create_settings_table.php
+++ database/migrations/create_settings_table.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace hackerESQ\Settings\database\migrations;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
